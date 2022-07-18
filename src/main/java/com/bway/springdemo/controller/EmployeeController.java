634555230068f4197a914b3f959325a52fcf5e71
Aashisna <aashisnathapaliya@gmail.com>
2022-07-18 16:57:13 +0545

    commit message

--- src/main/java/com/bway/springdemo/controller/EmployeeController.java
+++ src/main/java/com/bway/springdemo/controller/EmployeeController.java
@@ -1,10 +1,14 @@
 package com.bway.springdemo.controller;
 
+import java.security.Provider.Service;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.bway.springdemo.model.Employee;
 import com.bway.springdemo.repository.EmployeeRepository;
@@ -24,6 +28,30 @@ public class EmployeeController {
 		
 		empRepo.save(emp);
 		return "EmployeeForm";
+	}
+	@GetMapping("/list")
+	public String getAllEmployee( Model model)
+	{
+		model.addAttribute("emplist",empRepo.findAll());
+		return "EmployeeList";
+	}
+	@GetMapping("/delete")
+	public String delete(@RequestParam Long id)
+	{
+		empRepo.deleteById(id);
+		return "redirect:/list";
+	}
+	@GetMapping("/edit")
+	public String delete(@RequestParam Long id, Model model)
+	{
+		model.addAttribute("emodel",empRepo.getById(id));
+		return "EditForm";
+	}
+	@PostMapping("/update")
+	public String update(@ModelAttribute Employee emp)
+	{
+		empRepo.save(emp);
+		return "redirect:/list";
 	}
 
 }
--- src/main/java/com/bway/springdemo/repository/EmployeeRepository.java
+++ src/main/java/com/bway/springdemo/repository/EmployeeRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.bway.springdemo.model.Employee;
 import com.bway.springdemo.model.User;
 
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
 }
